100ed96b7c722ff4205245d4bf82bc06c9ebc58d
lo52_bishop <lo52@utbm.fr>
2017-12-20 19:02:03 +0100

    Résolution problème de git

--- ProjetDistrib/src/BlueMain.java
+++
@@ -1,9 +0,0 @@
-
-public class BlueMain {
-
-	public static void main(String[] args) {
-		
-		BlueRobot blue = new BlueRobot(true, 0.3f, 0.2f, 0.3f, 0.2f, 0.2f);
-		blue.run();
-	}
-}
--- ProjetDistrib/src/RedMain.java
+++
@@ -1,9 +0,0 @@
-
-public class RedMain {
-
-public static void main(String[] args) {
-		
-		RedRobot red = new RedRobot(true, 0.35f, 0.2f, 0.3f, 0.2f, 0.2f);
-		red.run();
-	}
-}
