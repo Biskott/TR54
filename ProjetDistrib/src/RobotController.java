fc51169e223508b744c9f3e4f696ccaf7405d88e
Sêdêgnon SESSOU <sessouhelmut@gmail.com>
2018-01-06 16:36:21 +0100

    robot controller version anglais

--- ProjetDistrib/src/RobotController.java
+++ ProjetDistrib/src/RobotController.java
@@ -112,3 +112,102 @@ public class RobotController {
 	}
 
 }
+
+
+//ma version écrite mais déjà en englais vu que le prof demande un code source en anglais de meme que les commentaire
+
+
+
+import lejos.robotics.RegulatedMotor;
+
+public class Pilot {
+	
+	private final RegulatedMotor leftMotor;
+	private final RegulatedMotor rightMotor;
+	private float forwardSpeed;
+	
+	
+	/*private final float wheelDiameter;
+	private final float wheelBase;*/
+	
+	/**
+	 * @param leftMotor
+	 * @param rightMotor
+	 * @param wheelDiameter
+	 * @param wheelBase
+	 */
+	public Pilot(RegulatedMotor leftMotor, RegulatedMotor rightMotor, float speed) {
+		this.leftMotor = leftMotor;
+		this.rightMotor = rightMotor;
+		this.setSpeed(speed);
+	}
+	
+	/**
+	 * Makes the robot goes forward in straight line
+	 */
+	public void forward() {
+		// Synchronize left and right motors
+		//this.leftMotor.synchronizeWith(new RegulatedMotor[] { this.rightMotor });
+		// All motors method called between startSynchronization and endSynchronization will be called
+		// simultaneously when endSynchronization is called
+		//this.leftMotor.startSynchronization();
+		this.leftMotor.forward();
+		this.rightMotor.forward();
+
+		//this.leftMotor.endSynchronization();
+	}
+	
+	/**
+	 * Stops the robot
+	 */
+	public void stop() {
+		// When immediate return is not set (or set to false), the instruction waits for effective stop of the motor
+		// When immediate return is set to true, the instruction is not blocking (i.e. the next instruction
+		// will be run without waiting for the motor to be effectively stopped)
+		this.leftMotor.stop(true);
+		this.rightMotor.stop();
+	}
+
+	/**
+	 * Make the robot Rotate to the left by setting the speed on motors.
+	 * In this case the leftMotor is going faster than rightMotor
+	 */
+	public void leftRotate()
+	{
+		this.leftMotor.setSpeed((int)((this.getSpeed()*0.4f)/100f * this.leftMotor.getMaxSpeed()));
+		this.rightMotor.setSpeed((int)((this.getSpeed()*0.8f)/100f * this.leftMotor.getMaxSpeed()));
+		this.forward();
+	}
+
+	/**
+	 * In this case rightMotor is going faster than leftMotor
+	 */
+	public void rightRotate()
+	{
+		this.leftMotor.setSpeed((int)((this.getSpeed()*0.8f)/100f * this.leftMotor.getMaxSpeed()));
+		this.rightMotor.setSpeed((int)((this.getSpeed()*0.4f)/100f * this.leftMotor.getMaxSpeed()));	
+		this.forward();
+	}
+	
+	/**
+	 * Sets the speed of the robot
+	 * @param speed the speed (from 0 for stop, to 100 for max speed)
+	 */
+	public void setSpeed(float speed) 
+	{
+		this.forwardSpeed = speed;
+		this.leftMotor.setSpeed((int)(speed / 100f * this.leftMotor.getMaxSpeed()));
+		this.rightMotor.setSpeed((int)(speed / 100f * this.rightMotor.getMaxSpeed()));
+	}
+	
+	public float getSpeed()
+	{
+		return this.forwardSpeed;
+	}
+	
+	public RegulatedMotor getLeftMotor() {
+		// TODO Auto-generated method stub
+		return this.leftMotor;
+	}
+
+}
