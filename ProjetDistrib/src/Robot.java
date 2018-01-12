a128fa0c9b137ba2fb289247ac4d8ca87068dd90
Bishop Florian <flo.electroyeur@gmail.com>
2018-01-12 17:06:36 +0100

    Update color detection

--- ProjetDistrib/src/Robot.java
+++ ProjetDistrib/src/Robot.java
@@ -20,8 +20,8 @@ public abstract class Robot {
 	// Robot direction
 	protected boolean direction;
 
-	// HashMap correspondant aux valeurs RGB des diff�rentes couleurs
-	protected HashMap<Color, ArrayList<Float>> colorList;
+	// HashMap which define colors caracteristics
+	protected HashMap<Color,float[]> colorList;
 
 	/**
 	 * Constructor
@@ -52,33 +52,33 @@ public abstract class Robot {
 	 */
 	protected void initializeColor(String colorToFollow) {
 
-		colorList = new HashMap<Color, ArrayList<Float>>();
+		colorList = new HashMap<Color, float[]>();
 
 		LCD.drawString("Put on the " + colorToFollow,1, 3);
 		LCD.drawString("track and press",1, 4);
 		LCD.drawString("any button",3, 5);
 		Button.waitForAnyPress();
-		colorList.put(Color.colorToFollow, new ArrayList<Float>(Arrays.asList(s.getColor())));
+		colorList.put(Color.colorToFollow, sensor.getColor());
 		LCD.drawString("Put on the black",1, 3);
 		LCD.drawString("track and press",1, 4);
 		LCD.drawString("any button",3, 5);
 		Button.waitForAnyPress();
-		colorList.put(Color.black, new ArrayList<Float>(Arrays.asList(s.getColor())));
+		colorList.put(Color.black, sensor.getColor());
 		LCD.drawString("Put on the white",1, 3);
 		LCD.drawString("track and press",1, 4);
 		LCD.drawString("any button",2, 5);
 		Button.waitForAnyPress();
-		colorList.put(Color.white, new ArrayList<Float>(Arrays.asList(s.getColor())));
+		colorList.put(Color.white, sensor.getColor());
 		LCD.drawString("Put on the pink ",1, 3);
 		LCD.drawString("marker and press",1, 4);
 		LCD.drawString("any button",3, 5);
 		Button.waitForAnyPress();
-		colorList.put(Color.pink, new ArrayList<Float>(Arrays.asList(s.getColor())));
+		colorList.put(Color.pink, sensor.getColor());
 		LCD.drawString("Put on the yellow",0, 3);
 		LCD.drawString("marker and press",1, 4);
 		LCD.drawString("any button",3, 5);
 		Button.waitForAnyPress();
-		colorList.put(Color.yellow, new ArrayList<Float>(Arrays.asList(s.getColor())));
+		colorList.put(Color.yellow, sensor.getColor());
 		LCD.clear();
 		LCD.drawString("Loading ...",1, 3);
 		Delay.msDelay(4000);
@@ -91,20 +91,20 @@ public abstract class Robot {
 	 * @param tab2 : RGB value of the first color
 	 * @return : distance between two colors
 	 */
-	private float getDistance(float[] tab1, float[] tab2) {
+	/*private float getDistance(float[] tab1, float[] tab2) {
 		float part1 = (tab2[0] - tab1[0]) * (tab2[0] - tab1[0]);
 		float part2 = (tab2[1] - tab1[1]) * (tab2[1] - tab1[1]);
 		float part3 = (tab2[2] - tab1[2]) * (tab2[2] - tab1[2]);
 
 		return (float) Math.sqrt(part1 + part2 + part3);
-	}
+	}*/
 
 	/**
 	 * Get the nearest color from the list of the detected color
 	 * @param value : RGB value of the detected color
 	 * @return : name of the nearest color
 	 */
-	public Color getColorLibelle(float[] value) {
+	/*public Color getColorLibelle(float[] value) {
 
 		Color colorDetected = Color.nothing;
 		float minDistance = Float.MAX_VALUE;
@@ -121,6 +121,26 @@ public abstract class Robot {
 			}
 		}
 		return colorDetected;
+	}*/
+	
+	/**
+	 * Get the best color from the list of the detected color thanks to fuzzies
+	 * @param value : RGB value of the detected color
+	 * @return : name of the nearest color
+	 */
+	public Color getColorLibelle(float[] value) {
+		if(isColorToFollow(value))
+			return Color.colorToFollow;
+		else if(isBlack(value))
+			return Color.black;
+		else if(isWhite(value))
+			return Color.white;
+		else if(isYellow(value))
+			return Color.yellow;
+		else if(isPink(value))
+			return Color.pink;
+		else
+			return Color.nothing;
 	}
 
 	/**
@@ -144,5 +164,95 @@ public abstract class Robot {
 		communication.sendInt(15);
 		Delay.msDelay(1000);
 		communication.waitData();
+	}
+	
+	/**
+	 * Fonction isBlack
+	 * Detect if the searched color is black with a particular fuzzy
+	 * @param color : color to check
+	 * @return : booleqn indicate if color is black
+	 */
+	protected boolean isBlack(float color[]) {
+		float[] blackTrack = colorList.get(Color.black);
+		float blackFuzzy = 0.2f;
+		if (color[0]<blackTrack[0]*(1+blackFuzzy) && color[0]>blackTrack[0]*(1-blackFuzzy) && 
+			color[1]<blackTrack[1]*(1+blackFuzzy) && color[1]>blackTrack[1]*(1-blackFuzzy) && 
+			color[2]<blackTrack[2]*(1+blackFuzzy) && color[2]>blackTrack[2]*(1-blackFuzzy) ) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+
+	/**
+	 * Fonction isWhite
+	 * Detect if the searched color is white with a particular fuzzy
+	 * @param color : color to check
+	 * @return : booleqn indicate if color is white
+	 */
+	protected boolean isWhite(float color[]) {
+		float[] whiteTrack = colorList.get(Color.white);
+		float whiteFuzzy = 0.2f;
+		if (color[0]<whiteTrack[0]*(1+whiteFuzzy) && color[0]>whiteTrack[0]*(1-whiteFuzzy) &&
+			color[1]<whiteTrack[1]*(1+whiteFuzzy) && color[1]>whiteTrack[1]*(1-whiteFuzzy) &&
+			color[2]<whiteTrack[2]*(1+whiteFuzzy) && color[2]>whiteTrack[2]*(1-whiteFuzzy) ) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+
+	/**
+	 * Fonction isColorToFollow
+	 * Detect if the searched color is the color to follow with a particular fuzzy
+	 * @param color : color to check
+	 * @return : booleqn indicate if color is the color to follow
+	 */
+	protected boolean isColorToFollow(float color[]) {
+		float[] colorTrack = colorList.get(Color.colorToFollow);
+		float colorFuzzy = 0.3f;
+		if (color[0]<colorTrack[0]*(1+colorFuzzy) && color[0]>colorTrack[0]*(1-colorFuzzy) &&
+			color[1]<colorTrack[1]*(1+colorFuzzy) && color[1]>colorTrack[1]*(1-colorFuzzy) &&
+			color[2]<colorTrack[2]*(1+colorFuzzy) && color[2]>colorTrack[2]*(1-colorFuzzy) ) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+
+	/**
+	 * Fonction isYellow
+	 * Detect if the searched color is yellow with a particular fuzzy
+	 * @param color : color to check
+	 * @return : booleqn indicate if color is yellow
+	 */
+	protected boolean isYellow(float color[]) {
+		float[] yellowMark = colorList.get(Color.yellow);
+		float yellowFuzzy = 0.2f;
+		if (color[0]<yellowMark[0]*(1+yellowFuzzy) && color[0]>yellowMark[0]*(1-yellowFuzzy) && 
+			color[1]<yellowMark[1]*(1+yellowFuzzy) && color[1]>yellowMark[1]*(1-yellowFuzzy) && 
+			color[2]<yellowMark[2]*(1+yellowFuzzy) && color[2]>yellowMark[2]*(1-yellowFuzzy) ) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+
+	/**
+	 * Fonction isPink
+	 * Detect if the searched color is pink with a particular fuzzy
+	 * @param color : color to check
+	 * @return : booleqn indicate if color is pink
+	 */
+	protected boolean isPink(float color[]) {
+		float[] pinkMark = colorList.get(Color.pink);
+		float pinkFuzzy = 0.3f;
+		if (color[0]<pinkMark[0]*(1+pinkFuzzy) && color[0]>pinkMark[0]*(1-pinkFuzzy) && 
+			color[1]<pinkMark[1]*(1+pinkFuzzy) && color[1]>pinkMark[1]*(1-pinkFuzzy) && 
+			color[2]<pinkMark[2]*(1+pinkFuzzy) && color[2]>pinkMark[2]*(1-pinkFuzzy) ) {
+			return true;
+		}else {
+			return false;
+		}
 	}
 }
